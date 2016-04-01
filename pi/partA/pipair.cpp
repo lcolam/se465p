537245ddd439359f643dfdab8b753fcb8d6acb5d
Leo Chak On Lam <lcolam@ecelinux5.uwaterloo.ca>
2016-03-31 20:47:49 -0400

    Accept command line arguments

--- pi/partA/pipair.cpp
+++ pi/partA/pipair.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <utility>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+const int T_SUPPORT = 3;
+const float T_CONFIDENCE = 0.65;
 
 int main(int argc, char *argv[]) {
-	return 0;
+    string bcFile;
+    if (argc >= 2) {
+        bcFile = argv[1];
+    }
+    int suppThres = argc >= 3 ? atoi(argv[2]) : T_SUPPORT;
+    float confThres = argc >= 4 ? atof(argv[3]) : T_CONFIDENCE;
+
+    map<string, int> support;
+    map<pair<string,string>, int> supportPair;
+
+    map<string, vector<string> > scopes;
+    map<pair<string,string>, vector<string> > scopesPair;
+
+    return 0;
 }
 
